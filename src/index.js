aacb0f71622b56d015cc82262ec1287910073f94
jordanmmartin <jdordan23@gmail.com>
2018-10-12 10:18:28 -0400

    great job team

--- src/index.js
+++ src/index.js
@@ -4,3 +4,18 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+
+let button = document.querySelector('button');
+button.addEventListener('click', () => fetchData())
+
+function fetchData() {
+  fetch('https://randomuser.me/api/')
+    .then(response => response.json())
+      .then(json => addPerson(json.results[0]))
+}
+
+function addPerson (person) {
+  let fullname = document.querySelector('#fullname')
+  fullname.innerText = `${person.name.first} ${person.name.last}`
+}
